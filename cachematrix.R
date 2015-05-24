7fa4d4eff03c7126b13ba94ece30a6b3b52ee387
Melissa Kistner <kistner.melissa@gmail.com>
2015-05-24 15:30:05 +0200

    Added some additional comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,26 @@
 ## This file contains functions for faster matrix inverion by
 ## making use cached values for inverses, where possible
 
-## makeCacheMatrix creates a special "matrix" object that can
-## cache its inverse
+## makeCacheMatrix makes three functions available: get, setInverse
+## and getInverse. These are used from within cacheSolve to 
+## compute the inverse of matrix x, using a cached inverse if it 
+## has been computed before.
 
 makeCacheMatrix <- function(x = matrix()) {
   stored_inv <- NULL
-  get <- function() {
+  # simply return the matrix
+  get <- function() { 
     x
   }
+  # store the matrix new_inv so that it may be accessed from cache
   setInverse <- function(new_inv) {
     stored_inv <<- new_inv
   }
+  # return the stored inverse
   getInverse <- function() {
     stored_inv
   }
+  # return the 3 functions that were defined
   list(get = get,
        setInverse = setInverse,
        getInverse = getInverse)
@@ -22,17 +28,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## cacheSolve computes the inverse of the special "matrix" x
-## returned by makeCacheMatrix
+## returned by makeCacheMatrix (the input for cacheSolve is the
+## output from makeCacheMatrix). cacheSolve uses the cached 
+## inverse instead of computing it again, if it has been computed 
+## before.
 
 cacheSolve <- function(x, ...) {
+  # get the stored inverse (NULL if it has not been computed before)
   stored_inv <- x$getInverse()
+  # if it is NULL (not computed before)
   if(is.null(stored_inv)) {
-    data <- x$get()
-    new_inv <- solve(data, ...) # calculate the inverse
-    x$setInverse(new_inv)
+    data <- x$get() # get the matrix x
+    new_inv <- solve(data, ...) # calculate its inverse
+    x$setInverse(new_inv) # store its inverse
     stored_inv <- new_inv
   }
-  else {
+  else { # the inverse already exists in 'stored_inv'
     message("Getting cached data...")
   }
   return(stored_inv)
